2620b991f094a38859d84ee823668e0147f118c5
hgelblat <hannah.gelblat@epfl.ch>
2021-12-12 18:06:27 +0100

    Update fci_test.R
    update fci test

--- code/fci_test.R
+++ code/fci_test.R
@@ -25,3 +25,49 @@ sample_200 <- sample_all[c(1:200),]
 #ne pas prendre les valeurs non définies (NA)
 sample<- drop_na(sample_200)
 
+#créer les data
+#matrice : sample (équivalent de "gmG$x" dans l'exemple)
+#graph :à créer
+###################################################
+
+
+#...
+
+###################################################
+op.orig <-
+  options(SweaveHooks=
+            list(fig=function() par(mar=c(5.1, 4.1, 1.1, 2.1))),
+          width = 75,
+          digits = 5,
+          prompt = "R> ")
+
+###################################################
+library("pcalg")
+#data("gmG")
+###################################################
+#pour cette étape on a besoin que la x ne soit
+#que composée de chiffres (pas des noms par ex homme ou femme)
+
+#################
+#convertir la matrice en dummies
+
+#...
+
+
+#################
+suffStat1 <- list(C = cor(gmL$x), n = nrow(gmL$x))
+pag.est <- fci(suffStat1, indepTest = gaussCItest,
+               p = ncol(gmL$x), alpha = 0.01, labels = as.character(2:5))
+par(mfrow = c(1,2))
+
+#pour la suite, créer le graph qui correspond aux données
+#################
+#créer le graphe
+
+#...
+
+
+#################
+
+plot(gmL$g, main = "")
+plot(pag.est)
